66216c37aebe74c4dd2958d5da8eb09c163c55f4
github username <github email address>
2017-08-21 17:49:14 -0400

    created adapter

---
+++ adapters/usersAdapter.js
@@ -0,0 +1,35 @@
+class UsersAdapter {
+  constructor() {
+    this.baseUrl = 'http://localhost:3000/api/v1/users'
+  }
+
+  getUsers() {
+    return fetch(this.baseUrl).then(resp => resp.json())
+    //fetch is a get request to the URL
+    //then it is parsed in JSON
+
+  }
+
+  createUser(name, email) {
+    const userCreateParams = {
+      method: 'POST',
+      headers: {
+        'Content-Type':'application/json'
+      },
+      body: JSON.stringify({users: {name: name, email: email}})
+    }
+    return fetch(this.baseUrl,userCreateParams).then( resp => resp.json() ).then(resp=>console.log(resp))
+  }
+
+}
+
+//   deleteUser(userId) {
+//     const deleteUrl = `${this.baseUrl}/${noteId}`
+//     const noteDeleteParams = {
+//       method: 'DELETE',
+//       headers: {
+//         'Content-Type':'application/json'
+//       }
+//       }
+//       return fetch(deleteUrl,noteDeleteParams).then( resp => resp.json() )
+//   }
